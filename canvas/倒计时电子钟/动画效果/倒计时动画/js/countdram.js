5e19da67ad58256df3a3797f73843cdb6cd2a226
邓进忠 <981705890@qq.com>
2018-12-19 16:34:54 +0800

    优化

--- canvas/倒计时电子钟/动画效果/倒计时动画/js/countdram.js
+++ canvas/倒计时电子钟/动画效果/倒计时动画/js/countdram.js
@@ -2,13 +2,12 @@ var WINDOW_WIDTH = 1024,
 	WINDOW_HEIGHT = 600,
 	CANVAS_COLOR = "rgb(0, 102, 153)",
 	CANVAS_RADIUS = 5,
-	CANVAS_INTERVAL = 1,
 	CANVAS_x = 350,
 	CANVAS_X = 150,
 	CANVAS_y = 50,
 	CANVAS_Y = 200;
 
-const endTime = new Date(2019, 0, 0, 0, 0, 0);
+const endTime = new Date(2019, 0, 0, 0, 5, 0);
 var curShow = 0;
 var balls = [];
 
@@ -19,14 +18,39 @@ const colors = ['#33B5E5', '#0099CC', '#AA66CC', '#9933CC', '#99CC00', '#669900'
 window.onload = function() {
 	var canvas = document.getElementById('canvas'),
 		context = canvas.getContext("2d");
+	
+	var h1EleHeight = document.getElementById('title').offsetHeight;
+		
+	WINDOW_WIDTH = document.body.clientWidth;
+	
+	if(WINDOW_WIDTH > 1000) {
+		WINDOW_HEIGHT = document.body.clientHeight - h1EleHeight
+	}else {
+		WINDOW_HEIGHT = (document.body.clientHeight - h1EleHeight) / 2;
+	}
+	
+    CANVAS_RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108)-1
+	
+	CANVAS_x = Math.round(WINDOW_WIDTH * 5 / 16)
+	CANVAS_y = CANVAS_RADIUS * 2
+	
+	CANVAS_X = Math.round(WINDOW_WIDTH /10);
+	CANVAS_Y = 13 * (CANVAS_RADIUS + 1) * 2;
+
+    
+
+   
+
 	canvas.width = WINDOW_WIDTH;
 	canvas.height = WINDOW_HEIGHT;
+
 	curShow = getShowTime();
+
 	setInterval(function() {
 		render(context);
 		upDate();
 	}, 50)
-	render(context);
+
 }
 
 function getShowTime() {
@@ -38,35 +62,38 @@ function getShowTime() {
 
 function upDate() {
 	var nextShowTime = getShowTime();
+
+	var x = CANVAS_X,
+		y = CANVAS_Y,
+		r = CANVAS_RADIUS;
+
 	var nextHours = parseInt(nextShowTime / 3600) % 24,
 		nextMinutes = parseInt((nextShowTime - nextHours * 3600) / 60),
-		nextSecinds = nextShowTime % 60,
-		curHours = parseInt(curShow / 3600) % 24,
+		nextSecinds = nextShowTime % 60;
+
+	var curHours = parseInt(curShow / 3600) % 24,
 		curMinutes = parseInt((curShow - curHours * 3600) / 60),
-		curSecinds = curShow % 60,
-		distance = 0;
-	if (curHours > 100) {
-		distance = 100;
-	}
+		curSecinds = curShow % 60;
+
 
 	if (nextSecinds != curSecinds) {
 		if (parseInt(curHours / 10) != parseInt(nextHours / 10)) {
-			addBalls(CANVAS_X + distance, CANVAS_Y, parseInt(curHours / 10));
+			addBalls(x + 0, y, parseInt(curHours / 10));
 		}
 		if (parseInt(curHours % 10) != parseInt(nextHours % 10)) {
-			addBalls(CANVAS_X + distance + 100, CANVAS_Y, parseInt(curHours % 10));
+			addBalls(x + 15 * (r + 1), y, parseInt(curHours % 10));
 		}
 		if (parseInt(curMinutes / 10) != parseInt(nextMinutes / 10)) {
-			addBalls(CANVAS_X + distance + 264, CANVAS_Y, parseInt(curMinutes / 10));
+			addBalls(x + 39 * (r + 1), y, parseInt(curMinutes / 10));
 		}
 		if (parseInt(curMinutes % 10) != parseInt(nextMinutes % 10)) {
-			addBalls(CANVAS_X + distance + 364, CANVAS_Y, parseInt(curMinutes % 10));
+			addBalls(x + 54 * (r + 1), y, parseInt(curMinutes % 10));
 		}
 		if (parseInt(curSecinds / 10) != parseInt(nextSecinds / 10)) {
-			addBalls(CANVAS_X + distance + 528, CANVAS_Y, parseInt(curSecinds / 10));
+			addBalls(x + 78 * (r + 1), y, parseInt(curSecinds / 10));
 		}
 		if (parseInt(curSecinds % 10) != parseInt(nextSecinds % 10)) {
-			addBalls(CANVAS_X + distance + 628, CANVAS_Y, parseInt(curSecinds % 10));
+			addBalls(x + 93 * (r + 1), y, parseInt(curSecinds % 10));
 		}
 		curShow = nextShowTime;
 	}
@@ -75,21 +102,26 @@ function upDate() {
 
 function addBalls(x, y, num) {
 	var r = CANVAS_RADIUS;
-	for (var i = 0; i < digit[num].length; i++) {
-		for (var j = 0; j < digit[num][i].length; j++) {
-			if (digit[num][i][j]) {
-				var aBall = {
-					x: x + j * 2 * (r + 1) + (r + 1),
-					y: y + i * 2 * (r + 1) + (r + 1),
-					g: 1.5 + Math.random(),
-					vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
-					vy: -5,
-					color: colors[Math.floor(Math.random() * colors.length)]
+	if (digit[num] instanceof Array) {
+		for (var i = 0; i < digit[num].length; i++) {
+			if (digit[num][i] instanceof Array) {
+				for (var j = 0; j < digit[num][i].length; j++) {
+					if (digit[num][i][j]) {
+						var aBall = {
+							x: x + j * 2 * (r + 1) + (r + 1),
+							y: y + i * 2 * (r + 1) + (r + 1),
+							g: 1.5 + Math.random(),
+							vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+							vy: -5,
+							color: colors[Math.floor(Math.random() * colors.length)]
+						}
+						balls.push(aBall);
+					}
 				}
-				balls.push(aBall);
 			}
 		}
 	}
+
 }
 
 function updateBallls() {
@@ -103,31 +135,48 @@ function updateBallls() {
 		}
 	}
 
+	var cnt = 0;
+	for (var i = 0; i < balls.length; i++) {
+		if (balls[i].x + CANVAS_RADIUS > 0 && balls[i].x - CANVAS_RADIUS < WINDOW_WIDTH) {
+			balls[cnt++] = balls[i];
+		}
+	}
+
+	while (balls.length > Math.min(300, cnt)) {
+		balls.pop();
+	}
+
 }
 
 function render(cxt) {
-	cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT)
+	cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	var dayX = CANVAS_x,
+		dayY = CANVAS_y;
+
+	var x = CANVAS_X,
+		y = CANVAS_Y,
+		r = CANVAS_RADIUS;
+
 	var hours = parseInt(curShow / 3600),
-		hoursEnd = hours % 24 % 10,
-		hoursStar = parseInt(hours % 24 / 10),
+		day = parseInt(hours / 24),
 		minutes = parseInt((curShow - hours * 3600) / 60),
-		minutesEnd = minutes % 10,
-		minutesStar = parseInt(minutes / 10),
-		secinds = curShow % 60,
-		secindsEnd = secinds % 10,
-		secindsStar = parseInt(secinds / 10),
-		drawIndex = 0,
-		drawX = CANVAS_X,
-		timeArray = [hoursStar, hoursEnd, 10, minutesStar, minutesEnd, 10, secindsStar, secindsEnd];
-	// 	if (hours > 100) {
-	// 		var hoursMiddle = parseInt(hours / 10) % 10;
-	// 		hoursStar = parseInt(hours / 100);
-	// 		timeArray.splice(0, 1, hoursStar, hoursMiddle);
-	// 	}
-	for (var i = 0; i < timeArray.length; i++) {
-		drawIndex = drawing(timeArray[i], cxt, drawX, CANVAS_Y);
-		drawX += (drawIndex + 1) * (CANVAS_RADIUS + 1) * 2
+		secinds = curShow % 60;
+
+	if (day > 10) {
+		drawing(dayX, dayY, parseInt(day / 10), cxt)
 	}
+	drawing(dayX + 15 * (r + 1), dayY, parseInt(day % 10), cxt);
+	drawing(dayX + 35 * (r + 1), dayY, 11, cxt);
+
+	drawing(x, y, parseInt(hours % 24 / 10), cxt);
+	drawing(x + 15 * (r + 1), y, parseInt(hours % 24 % 10), cxt);
+	drawing(x + 30 * (r + 1), y, 10, cxt);
+	drawing(x + 39 * (r + 1), y, parseInt(minutes / 10), cxt);
+	drawing(x + 54 * (r + 1), y, parseInt(minutes % 10), cxt);
+	drawing(x + 69 * (r + 1), y, 10, cxt);
+	drawing(x + 78 * (r + 1), y, parseInt(secinds / 10), cxt);
+	drawing(x + 93 * (r + 1), y, parseInt(secinds % 10), cxt);
 
 	for (var i = 0; i < balls.length; i++) {
 		cxt.fillStyle = balls[i].color;
@@ -138,39 +187,31 @@ function render(cxt) {
 		cxt.fill();
 
 	}
-	var day = parseInt(hours / 24),
-		dayStar = parseInt(day / 10),
-		dayEnd = day % 10,
-		drawx = CANVAS_x;
-	if (day > 10) {
-		drawIndex = drawing(dayStar, cxt, drawx, CANVAS_y);
-		drawx += (drawIndex + 1) * (CANVAS_RADIUS + 1) * 2
-	}
-	drawIndex = drawing(dayEnd, cxt, drawx, CANVAS_y);
-	drawx += (drawIndex + 1) * (CANVAS_RADIUS + 1) * 2
-	drawIndex = drawing(11, cxt, drawx, CANVAS_y);
-	drawx += (drawIndex + 1) * (CANVAS_RADIUS + 1) * 2
 
 }
 
 
-function drawing(sum, ctx, starX, starY) {
+function drawing(starX, starY, sum, ctx, ) {
 	var x = starX,
 		y = starY,
-		r = CANVAS_RADIUS,
-		interval = CANVAS_INTERVAL;
+		r = CANVAS_RADIUS;
+
 	ctx.fillStyle = CANVAS_COLOR;
-	for (var j = 0; j < digit[sum].length; j++) {
-		x = starX;
-		for (var i = 0; i < digit[sum][j].length; i++) {
-			if (digit[sum][j][i]) {
-				ctx.beginPath();
-				ctx.arc(x, y, r, 0, 2 * Math.PI)
-				ctx.fill();
+
+	if (digit[sum] instanceof Array) {
+		for (var j = 0; j < digit[sum].length; j++) {
+			x = starX;
+			if (digit[sum][j] instanceof Array) {
+				for (var i = 0; i < digit[sum][j].length; i++) {
+					if (digit[sum][j][i]) {
+						ctx.beginPath();
+						ctx.arc(x, y, r, 0, 2 * Math.PI)
+						ctx.fill();
+					}
+					x += 2 * (r + 1);
+				}
+				y += 2 * (r + 1);
 			}
-			x += 2 * (r + interval);
 		}
-		y += 2 * (r + interval);
 	}
-	return i;
 }
